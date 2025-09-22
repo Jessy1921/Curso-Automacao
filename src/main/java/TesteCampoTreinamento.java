ea19baae69f547778d4c84d12b671403e4f3e670
Jéssica <jessicaanjos806@gmail.com>
2025-09-22 15:52:33 -0300

    Exercicios até a aula 28

---
+++ src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,101 @@
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TesteCampoTreinamento {
+	
+@Test
+public void testeTextField() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+    driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
+    Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+    
+    driver.quit();
+}
+
+@Test
+public void deveInteragirComTextArea() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste\n\naasldjdlks\nUltima linha");
+	Assert.assertEquals("teste\n\naasldjdlks\nUltima linha", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+	
+	driver.quit();
+}
+
+@Test
+public void deveInteragirComRadioButton() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.findElement(By.id("elementosForm:sexo:0")).click();
+	Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+	driver.quit();
+}
+
+@Test
+public void deveInteragirComCheckbox() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+	Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+	driver.quit();
+  }
+
+@Test
+public void deveInteragirComCombo() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+	Select combo = new Select(element);
+	//combo.selectByIndex(3);
+	//combo.selectByValue("superior");
+	combo.selectByVisibleText("2o grau completo");
+	
+	Assert.assertEquals("2o grau completo", combo.getFirstSelectedOption().getText());
+	driver.quit();
+	}
+
+@Test
+public void deveVerificarValoresCombo() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+	Select combo = new Select(element);
+	List<WebElement> options = combo.getOptions();
+	Assert.assertEquals(8, options.size());
+	
+	boolean encontrou = false;
+	for(WebElement option: options) {
+		if(option.getText().equals("Mestrado")) {
+			encontrou = true;
+			break;
+		}
+	}
+	Assert.assertTrue(encontrou);
+}
+
+
+}
+
+
+
