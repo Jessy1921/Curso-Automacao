a07c0a45e7defa3867233ede0b76aedb997fe40f
Jessy1921 <jessicaanjos806@gmail.com>
2025-09-24 15:51:20 -0300

    Aulas 30 e 31

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,6 +1,8 @@
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -129,12 +131,40 @@ public void deveInteragirComBotoes () {
 	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 	WebElement botao = driver.findElement(By.id("buttonSimple"));
 	botao.click();
-	
 	Assert.assertEquals("Obrigado!", botao.getAttribute("value"));
 	driver.quit();
 	
 }
 
+@Test 
+@Ignore
+public void deveInteragirComLinks() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	
+	driver.findElement(By.linkText("Voltar")).click();
+	
+	Assert.assertEquals("Voltou!", driver.findElement(By.id("resultado")).getText());
+	driver.quit();
+	
+}
+
+@Test
+public void deveBuscarTextosNaPagina() {
+	System.setProperty("webdriver.gecko.driver", "C:\\JAVA_HOME\\Automação_home\\geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.manage().window().setSize(new Dimension(1200, 765));
+	driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	
+	//Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("Campo de Treinamento"));
+	Assert.assertEquals("Campo de Treinamento", driver.findElement(By.tagName("h3")).getText());
+	
+	Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", driver.findElement(By.className("facilAchar")).getText());
+
+	driver.quit();
+	}
 }
 
 
