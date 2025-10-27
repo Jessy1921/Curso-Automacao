7d608440aa82492fe95d1dc2e1b8fa19fae78352
jessica.araujo <Jessica.araujo@planejameno.rj.gov.br>
2025-10-27 17:43:58 -0300

    erros para resolver ainda

---
+++ src/main/java/br/ce/wcaquino/tests/ContaTest.java
@@ -0,0 +1,56 @@
+package br.ce.wcaquino.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.pages.ContasPage;
+import br.ce.wcaquino.pages.MenuPage;
+
+public class ContaTest extends BaseTest {
+	
+	MenuPage menuPage = new MenuPage();
+	ContasPage contasPage = new ContasPage();
+	
+	@Test
+	public void testInserirConta(){
+		menuPage.acessarTelaInserirConta();
+		
+		contasPage.setNome("Conta do Teste");
+		contasPage.salvar();
+		
+		Assert.assertEquals("Conta adicionada com sucesso!", contasPage.obterMensagemSucesso());
+	}
+	
+	@Test
+	public void testAlterarConta(){
+		menuPage.acessarTelaListarConta();
+		
+		contasPage.clicarAlterarConta("Conta do Teste");
+		
+		contasPage.setNome("Conta do Teste alterada");
+		contasPage.salvar();
+		
+		Assert.assertEquals("Conta alterada com sucesso!", contasPage.obterMensagemSucesso());
+	}
+	
+	@Test
+	public void testInserirContaMesmoNome(){
+		menuPage.acessarTelaInserirConta();
+		
+		contasPage.setNome("Conta do Teste alterada");
+		contasPage.salvar();
+		
+		Assert.assertEquals("Já existe uma conta com esse nome!", contasPage.obterMensagemErro());
+	}
+	
+	@Test
+	public void testExcluirContaComMovimentacao(){
+		menuPage.acessarTelaListarConta();
+		
+		contasPage.clicarExcluirConta("Conta do Teste alterada");
+		
+		Assert.assertEquals("Conta em uso na movimentações", contasPage.obterMensagemErro());
+	}
+
+}
